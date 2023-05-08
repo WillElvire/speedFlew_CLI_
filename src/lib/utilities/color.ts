b783e5d13b5a7217886a636ca780d455d7758062
macbook <wkoua@oxylead.net>
2023-05-08 09:03:25 +0000

    feat : console applications

--- src/lib/utilities/color.ts
+++ src/lib/utilities/color.ts
@@ -1,5 +1,24 @@
-import chalk from "chalk";
+import { consoleColor } from "../../core/types/console";
+const c = require("ansi-colors");
 
-export function defineColorToConsole (text : string) {
-  return console.log(chalk.blue(text));
-};
+export function defineColorToConsole(
+  text: string,
+  color: consoleColor = "cyan"
+) {
+  return applyColor(text, color);
+}
+
+function applyColor(text: string, color: consoleColor) {
+  switch (color) {
+    case "cyan":
+      return console.log(c.cyan(text));
+    case "red":
+      return console.log(c.red(text));
+
+    case "yellow":
+      return console.log(c.yellow(text));
+
+    case "blue":
+      return console.log(c.blue(text));
+  }
+}
