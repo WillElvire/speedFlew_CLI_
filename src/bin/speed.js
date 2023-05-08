0655c0ce2b1cbae9207e3e054ff10a44797e6f7d
macbook <wkoua@oxylead.net>
2023-05-08 07:07:59 +0000

    feat : refractor

---
+++ src/bin/speed.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+'use strict';
+
+import { SpeedFlewConsole } from '../lib/bootstrap.ts';
+
+const consoleApplication = new SpeedFlewConsole();
+consoleApplication.runApplication();
---
+++ src/lib/bootstrap.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+"use strict";
+
+export class SpeedFlewConsole {
+  constructor() {}
+  runApplication() {
+    console.log("spee")
+  }
+}
+
